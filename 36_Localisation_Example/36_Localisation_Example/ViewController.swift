8d81ce0d34c5c7bf6009d74e2cdffe7d1db89941
fedir72 <fedir72@ua.fm>
2020-03-25 14:04:46 +0200

    start project

---
+++ 36_Localisation_Example/36_Localisation_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  36_Localisation_Example
+//
+//  Created by fedir on 25.03.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
